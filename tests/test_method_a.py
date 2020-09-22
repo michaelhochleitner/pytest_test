e8e4e754bd44e5edb1fe27f2d69e57fdbf1ade26
Michael Hochleitner <hochleitner@protonmail.com>
2020-09-22 12:32:03 +0200

    Refactor variable name

--- tests/test_method_a.py
+++ tests/test_method_a.py
@@ -2,16 +2,16 @@ import pytest
 
 
 @pytest.fixture
-def document():
+def someclass():
     from package.someclass import SomeClass
     return SomeClass()
 
 
-def test_method_a_one(document):
-    result = document.method_a("a")[0]
+def test_method_a_one(someclass):
+    result = someclass.method_a("a")[0]
     assert result.string == "a"
 
 
-def test_method_a_two(document):
-    result = document.method_a("b")[0]
+def test_method_a_two(someclass):
+    result = someclass.method_a("b")[0]
     assert result.string == "b"
