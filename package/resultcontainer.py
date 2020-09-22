c21073085bb04b7978130ee83f267a3e8f5cff9b
Michael Hochleitner <hochleitner@protonmail.com>
2020-09-22 12:27:06 +0200

    Use regular classes and fixture

--- package/resultcontainer.py
+++ package/resultcontainer.py
@@ -1,7 +1,3 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class ResultContainer:
     """SomeClass.method_a() appends values to the list in this class."""
 
--- tests/test_method_a.py
+++ tests/test_method_a.py
@@ -1,20 +1,17 @@
-from dataclasses import asdict
-
 import pytest
 
 
 @pytest.fixture
 def document():
     from package.someclass import SomeClass
-    class_instance = SomeClass()
-    return class_instance
+    return SomeClass()
 
 
 def test_method_a_one(document):
-    result = asdict(document.method_a("a")[0])
-    assert result["string"] == "a"
+    result = document.method_a("a")[0]
+    assert result.string == "a"
 
 
 def test_method_a_two(document):
-    result = asdict(document.method_a("b")[0])
-    assert result["string"] == "b"
+    result = document.method_a("b")[0]
+    assert result.string == "b"
